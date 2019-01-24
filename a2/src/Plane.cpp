189ee81518d93c27cf5394cca05a440740e26357
ANALOG\gliu3 <Guanxiong.Liu@analog.com>
2019-01-24 15:21:44 -0500

    update

--- a2/src/Plane.cpp
+++ a2/src/Plane.cpp
@@ -6,7 +6,21 @@ bool Plane::intersect(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
-  return false;
+  Eigen::Vector3d e = ray.origin;
+  Eigen::Vector3d d = ray.direction;
+  double result = (point - e).dot(normal) / d.dot(normal);
+
+  if (d.dot(normal) == 0){
+    return false;
+  }
+  else if (result < min_t){
+    return false;
+  }
+  else if (result >= min_t){
+    t = result;
+    n = normal;
+    return true;
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
 
