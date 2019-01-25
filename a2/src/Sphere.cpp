881fc35ac50bf7c7edb589798e1730eeb1c23d90
Guanxiong Liu <guanxiong.liu@mail.utoronto.ca>
2019-01-25 18:17:54 -0500

    update

--- a2/src/Sphere.cpp
+++ a2/src/Sphere.cpp
@@ -1,67 +1,77 @@
 #include "Sphere.h"
 #include "Ray.h"
 #include <cmath>
+#include <math.h>
+#include <iostream>
 
 bool Sphere::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
+  Eigen::Vector3d e = ray.origin;
+  Eigen::Vector3d d = ray.direction;
+  Eigen::Vector3d c = center;
+  bool return_val = false;
+  // compute k**2
+  double k_2 = (d).dot(e - c) * (d).dot(e - c) - d.dot(d) * ((e - c).dot((e - c)) - radius * radius);
 
-  Eigen::Vector3d to_center = center - ray.origin;
+  if (k_2 >= 0) {
+    // valid
+    double k = sqrt(k_2);
+    double t1 = (-(d).dot(e - c) + k) / d.dot(d);
+    double t2 = (-(d).dot(e - c) - k) / d.dot(d);
 
-  Eigen::Vector3d projection = ray.direction.normalized() * to_center.dot(ray.direction.normalized());
+    double min = fmin(t1, t2);
+    double max = fmax(t1, t2);
 
-  Eigen::Vector3d orthogonal = to_center - projection;
-
-  if(orthogonal.norm() > radius){//ray misses the sphere
-    return false;
+    if (min > min_t) {
+      t = min;
+      return_val = true;
+    } else if (min < min_t && max > min_t) {
+      t = max;
+      return_val = true;
+    }
   }
-  else{
-    double x1 = sqrt(radius*radius - orthogonal.norm()*orthogonal.norm());
-    double x2 = -x1;
-    Eigen::Vector3d intersection_point = ray.origin + projection - x1*(projection.normalized());
-    n = (intersection_point - center).normalized();
-    t = (intersection_point - ray.origin).norm()/ray.direction.norm();
-    if(t > min_t){
+
+  if(return_val == true) {
+      Eigen::Vector3d p = e + t * d;
+      n = (p - c) / radius;
+  }
+
+  return return_val;
+  // Replace with your code here:
+  Eigen::Vector3d d = ray.direction;
+  Eigen::Vector3d e = ray.origin;
+  Eigen::Vector3d c = center;
+
+  double discriminant = pow(d.dot(e - c), 2) - d.dot(d) * ((e - c).dot(e - c) - pow(radius, 2));
+  if (discriminant == 0.0){
+    double result = -d.dot(e-c)/d.dot(d);
+    if (result >= min_t){
+      t = result;
+      n = (e + t * d - c) / radius; //unit n = (p-c)/radius
       return true;
     }
     else{
       return false;
     }
   }
-  // Replace with your code here:
-  // Eigen::Vector3d d = ray.direction;
-  // Eigen::Vector3d e = ray.origin;
-  // Eigen::Vector3d c = center;
-
-  // double discriminant = pow(d.dot(e - c), 2) - d.dot(d) * ((e - c).dot(e - c) - pow(radius, 2));
-  // if (discriminant == 0.0){
-  //   double result = -d.dot(e-c)/d.dot(d);
-  //   if (result >= min_t){
-  //     t = result;
-  //     n = (e + t * d - c) / radius; //unit n = (p-c)/radius
-  //     return true;
-  //   }
-  //   else{
-  //     return false;
-  //   }
-  // }
-  // else if (discriminant > 0.0){
-  //   double root_1 = (-d.dot(e-c)+sqrt(discriminant))/d.dot(d);
-  //   double root_2 = (-d.dot(e-c)-sqrt(discriminant))/d.dot(d);
-  //   double minimum = fmin(root_1, root_2);
-  //   if (minimum >= min_t){
-  //     t = minimum;
-  //     n = (e + t * d - c) / radius;
-  //     return true;
-  //   }
-  //   else{
-  //     return false;
-  //   }
-  // }
-  // else{
-  //   return false;
-  // }
+  else if (discriminant > 0.0){
+    double root_1 = (-d.dot(e-c)+sqrt(discriminant))/d.dot(d);
+    double root_2 = (-d.dot(e-c)-sqrt(discriminant))/d.dot(d);
+    double minimum = fmin(root_1, root_2);
+    if (minimum >= min_t){
+      t = minimum;
+      n = (e + t * d - c) / radius;
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  else{
+    return false;
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
 
