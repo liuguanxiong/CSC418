d64b8d072ec386aae18182ca46a3a428b0b0f6a6
Guanxiong Liu <guanxiong.liu@mail.utoronto.ca>
2019-01-25 13:16:08 -0500

    update

--- a2/src/Triangle.cpp
+++ a2/src/Triangle.cpp
@@ -6,44 +6,6 @@ bool Triangle::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Eigen::Vector3d A = std::get<0>(corners);
-  // Eigen::Vector3d B = std::get<1>(corners);
-  // Eigen::Vector3d C = std::get<2>(corners);
-
-  // Eigen::MatrixXd mat(3, 3);
-  // mat(0, 0) = A[0]-B[0];
-  // mat(0, 1) = A[0]-C[0];
-  // mat(0, 2) = ray.direction[0];
-  // mat(1, 0) = A[1]-B[1];
-  // mat(1, 1) = A[1]-C[1];
-  // mat(1, 2) = ray.direction[1];
-  // mat(2, 0) = A[2]-B[2];
-  // mat(2, 1) = A[2]-C[2];
-  // mat(2, 2) = ray.direction[2];
-
-  // Eigen::Vector3d ori(A[0] - ray.origin[0], A[1] - ray.origin[1], A[2] - ray.origin[2]);
-  // Eigen::Vector3d ans = mat.colPivHouseholderQr().solve(ori);
-  // bool hit;
-  // t = ans[2];
-
-  // if (t < min_t){
-  //   hit = false;
-  // }
-  // else if (ans[1] < 0 || ans[1] > 1){
-  //   hit = false;
-  // }
-  // else if (ans[0] < 0 || ans[0] > 1 - ans[1]){
-  //   hit = false;
-  // }
-  // else{
-  //   Eigen::Vector3d pt = ray.origin + t * ray.direction;
-  //   Eigen::Vector3d line1 = pt - A;
-  //   Eigen::Vector3d line2 = pt - B;
-  //   n = line1.cross(line2);
-  //   n.normalized();
-  //   hit = true;
-  // }
-  // return hit;
   // Replace with your code here:
   Eigen::Vector3d A = std::get<0>(corners);
   Eigen::Vector3d B = std::get<1>(corners);
