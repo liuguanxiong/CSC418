16be1f3f5c89e0ff3003f3eba4f4ef454ea6ce92
Guanxiong Liu <guanxiong.liu@mail.utoronto.ca>
2019-02-14 16:57:08 -0500

    update

--- a5/src/sphere.cpp
+++ a5/src/sphere.cpp
@@ -13,5 +13,32 @@ void sphere(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
+  int total_faces = num_faces_u * num_faces_v;
+  V.resize(total_faces, 3);
+  F.resize(total_faces, 4);
+  UV.resize(total_faces, 2);
+  UF.resize(total_faces, 4);
+  NV.resize(total_faces, 3);
+  NF.resize(total_faces, 4);
+
+  int count = 0;
+  for (int i = 0; i < num_faces_u; i++){
+    for (int j = 0; j < num_faces_v; j++){
+
+      double x, y, z, phi, theta;
+      phi = i / num_faces_u * M_PI;
+      theta = j / num_faces_v * 2 * M_PI;
+      x = sin(phi) * cos(theta);
+      y = sin(phi) * sin(theta);
+      z = cos(phi);
+
+      int ver1 = i * (num_faces_v)
+
+      V.row(count) = Eigen::RowVector3d(x, y, z);
+      UV.row(count) = Eigen::RowVector3d(i / num_faces_u, j / num_faces_v);
+      NV.row(count) = Eigen::RowVector3d(x, y, z);
+      count++;
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
